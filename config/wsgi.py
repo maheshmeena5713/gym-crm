1c81d2a8ad141ecfa5057143712b316ed0ef3620
mahesh242 <maheshmeena6458@gmail.com>
2026-02-21 12:34:00 +0530

    feat: Implement dotenv for environment variable loading and default manage.py to production settings.

--- config/wsgi.py
+++ config/wsgi.py
@@ -4,6 +4,9 @@ WSGI config for Gym AI SaaS.
 
 import os
 from django.core.wsgi import get_wsgi_application
+from dotenv import load_dotenv
+
+load_dotenv()
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.production')
 application = get_wsgi_application()
